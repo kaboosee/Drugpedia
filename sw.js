9f7c04e536fc528f9adfefb3fdec7cb215626d3b
Calum Pryce <calum1234.pryce@gmail.com>
2026-06-10 18:09:15 +0100

    ui fixes

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 // Bump this version whenever any cached asset changes, or returning
 // visitors will keep the old files (see README "Releasing changes").
-const CACHE = "drugpedia-v2";
+const CACHE = "drugpedia-v3";
 
 const ASSETS = [
   "./",
